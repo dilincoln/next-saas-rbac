72fd55e556bec420f34860fc2b015691307a1c1c
Diego Lincoln <diego@mutualtecnologia.com.br>
2024-08-14 08:23:32 -0300

    chore: update package and configuration names to @saas namespace

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,9 +1,9 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@repo/eslint-config/next.js"],
+  extends: ["@saas/eslint-config/next.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
   },
-};
+}
