97b3a3de39d457dbdf28173e6cda0035899018cd
Diego Lincoln <diego@mutualtecnologia.com.br>
2024-09-01 19:20:29 -0300

    feat: add swagger integration for API documentation and update TypeScript config to use CommonJS module format

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,6 +1,9 @@
 import fastifyCors from '@fastify/cors'
+import fastifySwagger from '@fastify/swagger'
+import fastifyScalarUI from '@scalar/fastify-api-reference'
 import fastify from 'fastify'
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
@@ -12,6 +15,26 @@ const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Next.js SaaS API',
+      description: 'Full-stack SaaS app with multi-tenant & RBAC',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifyScalarUI, {
+  routePrefix: '/docs',
+  configuration: {
+    spec: {
+      content: () => app.swagger(),
+    },
+  },
+})
 
 app.register(fastifyCors)
 
