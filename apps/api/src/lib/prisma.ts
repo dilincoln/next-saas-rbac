2125e59c231384d8037200ce0751098ac35554a1
Diego Lincoln <diego@mutualtecnologia.com.br>
2024-09-12 16:30:23 -0300

    feat: enhance PrismaClient logging to include query events and duration for improved debugging and performance monitoring

--- apps/api/src/lib/prisma.ts
+++ apps/api/src/lib/prisma.ts
@@ -1,5 +1,24 @@
+import type { Prisma } from '@prisma/client'
 import { PrismaClient } from '@prisma/client'
 
 export const prisma = new PrismaClient({
-  log: ['query'],
+  log: [
+    {
+      emit: 'event',
+      level: 'query',
+    },
+  ],
+})
+
+prisma.$on('query', (e: Prisma.QueryEvent) => {
+  console.log(
+    'Query:',
+    e.params
+      ? e.query.replace(
+          /\$(\d+)/g,
+          (value, i) => JSON.parse(e.params)[i - 1] || value,
+        )
+      : e.query,
+  )
+  console.log('Duration:', e.duration + 'ms')
 })
