f3dc0d4be0e5eaeeb1e13a004c09054d339b0c77
Diego Lincoln <diego@mutualtecnologia.com.br>
2024-10-01 00:45:38 -0300

    feat: add remove-member route to allow deletion of members with permissions validation in the API

---
+++ apps/api/src/http/routes/members/remove-member.ts
@@ -0,0 +1,55 @@
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { authMiddleware } from '@/http/middlewares/auth-middleware'
+import { prisma } from '@/lib/prisma'
+import { getUserPermissions } from '@/utils/get-user-permissions'
+
+import { BadRequestError } from '../_errors/bad-request-error'
+
+export async function removeMember(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(authMiddleware)
+    .delete(
+      '/organizations/:orgSlug/members/:memberId',
+      {
+        schema: {
+          tags: ['members'],
+          summary: 'Remove a member from the organization',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            orgSlug: z.string(),
+            memberId: z.string().uuid(),
+          }),
+          response: {
+            204: z.null(),
+          },
+        },
+      },
+      async (request, reply) => {
+        const userId = await request.getCurrentUserId()
+        const { orgSlug, memberId } = request.params
+        const { membership, organization } =
+          await request.getUserMembership(orgSlug)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('delete', 'User')) {
+          throw new BadRequestError(
+            "You're not allowed to remove this member from the organization",
+          )
+        }
+
+        await prisma.member.delete({
+          where: {
+            id: memberId,
+            organizationId: organization.id,
+          },
+        })
+
+        return reply.status(204).send()
+      },
+    )
+}
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -18,6 +18,7 @@ import { getProfile } from '@/http/routes/auth/get-profile'
 import { requestPasswordRecover } from '@/http/routes/auth/request-password-recover'
 import { resetPassword } from '@/http/routes/auth/reset-password'
 import { getMembers } from '@/http/routes/members/get-members'
+import { removeMember } from '@/http/routes/members/remove-member'
 import { updateMember } from '@/http/routes/members/update-member'
 import { createOrganization } from '@/http/routes/orgs/create-organization'
 import { getMembership } from '@/http/routes/orgs/get-membership'
@@ -96,6 +97,7 @@ app.register(updateProject)
 
 app.register(getMembers)
 app.register(updateMember)
+app.register(removeMember)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server listening on port 3333')
