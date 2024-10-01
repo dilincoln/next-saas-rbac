2907d8edc69e29cd113af88f32886e6ce438fbe9
Diego Lincoln <diego@mutualtecnologia.com.br>
2024-09-30 23:31:47 -0300

    feat: implement shutdown-organization route with validation and permissions check for organization deletion in the API

---
+++ apps/api/src/http/routes/orgs/shutdown-organization.ts
@@ -0,0 +1,57 @@
+import { organizationSchema } from '@saas/auth'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { authMiddleware } from '@/http/middlewares/auth-middleware'
+import { prisma } from '@/lib/prisma'
+import { getUserPermissions } from '@/utils/get-user-permissions'
+
+import { BadRequestError } from '../_errors/bad-request-error'
+
+export async function shutdownOrganization(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(authMiddleware)
+    .delete(
+      '/organizations/:slug',
+      {
+        schema: {
+          tags: ['organizations'],
+          summary: 'Shutdown organization',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            204: z.null(),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug } = request.params
+
+        const userId = await request.getCurrentUserId()
+        const { membership, organization } =
+          await request.getUserMembership(slug)
+
+        const authOrganization = organizationSchema.parse(organization)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('delete', authOrganization)) {
+          throw new BadRequestError(
+            "You're not allowed to shutdown this organization",
+          )
+        }
+
+        await prisma.organization.delete({
+          where: {
+            id: organization.id,
+          },
+        })
+
+        return reply.status(204).send()
+      },
+    )
+}
