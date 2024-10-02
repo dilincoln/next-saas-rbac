61e039b1859b9a34a26a999210ccf3f792dcf26f
Diego Lincoln <diego@mutualtecnologia.com.br>
2024-10-02 00:46:21 -0300

    feat: add eslint-plugin-react and configure ESLint rules for React to improve code quality and maintainability in the project

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,13 @@
-import './globals.css'
+import '@/app/globals.css'
 
+import clsx from 'clsx'
 import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -12,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html className={clsx(inter.className, 'dark')} lang="en">
       <body>{children}</body>
     </html>
   )
