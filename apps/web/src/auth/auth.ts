9940852c3e3d8dc1e1b0848481e63721cecca938
Diego Lincoln <diego@mutualtecnologia.com.br>
2024-11-11 01:15:15 -0300

    feat: add auth function to redirect unauthenticated users to sign-in and handle profile retrieval in auth module

--- apps/web/src/auth/auth.ts
+++ apps/web/src/auth/auth.ts
@@ -1,7 +1,28 @@
+import { getProfile } from '@/http/get-profile'
 import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 
 export async function isAuthenticated() {
   const { get } = await cookies()
 
   return !!get('token')?.value
+}
+
+export async function auth() {
+  const { get } = await cookies()
+  const token = get('token')?.value
+
+  if (!token) {
+    redirect('/auth/sign-in')
+  }
+
+  try {
+    const { user } = await getProfile()
+
+    return { user }
+  } catch (err) {
+    console.log(err)
+  }
+
+  redirect('/api/auth/sign-out')
 }
