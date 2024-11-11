8711421269ed8b67f5a8dcb9f7b5f354f89a73ca
Diego Lincoln <diego@mutualtecnologia.com.br>
2024-11-10 22:27:20 -0300

    feat: implement ApiClient class for handling HTTP requests with methods

---
+++ apps/web/src/http/api-client.ts
@@ -0,0 +1,81 @@
+import { env } from '@saas/env'
+
+class ApiClient {
+  private baseUrl = env.NEXT_PUBLIC_API_URL
+
+  private async request<R>(url: string, config?: RequestInit) {
+    const request = await fetch(new URL(url, this.baseUrl).toString(), {
+      ...config,
+      headers: {
+        ...config?.headers,
+        'Content-Type': 'application/json',
+      },
+    })
+
+    const response = await request.json()
+
+    if (!request.ok) {
+      throw new Error(response.message)
+    }
+
+    return response as Promise<R>
+  }
+
+  public get<ResponseType>(
+    url: string,
+    config?: Omit<RequestInit, 'method' | 'body'>
+  ) {
+    return this.request<ResponseType>(url, {
+      method: 'GET',
+      ...config,
+    })
+  }
+
+  public post<RequestType, ResponseType>(
+    url: string,
+    data: RequestType,
+    config?: Omit<RequestInit, 'method' | 'body'>
+  ) {
+    return this.request<ResponseType>(url, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      ...config,
+    })
+  }
+
+  public put<RequestType, ResponseType>(
+    url: string,
+    data: RequestType,
+    config?: Omit<RequestInit, 'method' | 'body'>
+  ) {
+    return this.request<ResponseType>(url, {
+      method: 'PUT',
+      body: JSON.stringify(data),
+      ...config,
+    })
+  }
+
+  public delete<ResponseType>(
+    url: string,
+    config?: Omit<RequestInit, 'method' | 'body'>
+  ) {
+    return this.request<ResponseType>(url, {
+      method: 'DELETE',
+      ...config,
+    })
+  }
+
+  public patch<RequestType, ResponseType>(
+    url: string,
+    data: RequestType,
+    config?: Omit<RequestInit, 'method' | 'body'>
+  ) {
+    return this.request<ResponseType>(url, {
+      method: 'PATCH',
+      body: JSON.stringify(data),
+      ...config,
+    })
+  }
+}
+
+export const api = new ApiClient()
