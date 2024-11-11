fe1b04f790a88f1e7310fc6b2fe11eeb67fb44f7
Diego Lincoln <diego@mutualtecnologia.com.br>
2024-11-11 01:14:54 -0300

    feat: implement sign-out route to delete token cookie and redirect to sign-in page in auth module

---
+++ apps/web/src/app/api/auth/sign-out/route.ts
@@ -0,0 +1,14 @@
+import { cookies } from 'next/headers'
+import { NextResponse, type NextRequest } from 'next/server'
+
+export async function GET(request: NextRequest) {
+  const redirectUrl = request.nextUrl.clone()
+
+  redirectUrl.pathname = '/auth/sign-in'
+
+  const { delete: deleteCookie } = await cookies()
+
+  deleteCookie('token')
+
+  return NextResponse.redirect(redirectUrl)
+}
