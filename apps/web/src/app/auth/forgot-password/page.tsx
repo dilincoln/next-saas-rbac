269a1b4d9273d7e92a02a9e284b9810997381342
Diego Lincoln <diego@mutualtecnologia.com.br>
2024-11-16 01:42:43 -0300

    feat: integrate i18n messages for forgot password page

--- apps/web/src/app/auth/forgot-password/page.tsx
+++ apps/web/src/app/auth/forgot-password/page.tsx
@@ -1,3 +1,4 @@
+import * as m from '@saas/i18n/messages'
 import Link from 'next/link'
 
 import { Button } from '@/components/ui/button'
@@ -8,17 +9,17 @@ export default function ForgotPasswordPage() {
   return (
     <form action="" className="space-y-4">
       <div className="space-y-1">
-        <Label htmlFor="email">E-mail</Label>
+        <Label htmlFor="email">{m.email()}</Label>
         <Input id="email" name="email" type="email" />
       </div>
 
       <Button className="w-full" type="submit">
-        Recover password
+        {m.recover_password()}
       </Button>
 
       <div className="flex w-full justify-center">
         <Button asChild size="sm" variant="link">
-          <Link href="/auth/sign-up">Sign in instead</Link>
+          <Link href="/auth/sign-up">{m.sign_in_instead()}</Link>
         </Button>
       </div>
     </form>
