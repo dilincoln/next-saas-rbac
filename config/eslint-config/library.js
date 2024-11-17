66aba463f1ab4e5c3a6a6132968104c2b3f7f13d
Diego Lincoln <diego@mutualtecnologia.com.br>
2024-11-17 00:30:02 -0300

    feat: enhance ESLint config by integrating TypeScript support and updating rules for improved code quality and sorting functionality

--- config/eslint-config/library.js
+++ config/eslint-config/library.js
@@ -1,8 +1,10 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   extends: ['@rocketseat/eslint-config/react'],
-  plugins: ['simple-import-sort'],
+  plugins: ['simple-import-sort', '@typescript-eslint'],
   rules: {
     'simple-import-sort/imports': 'error',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': 'error',
   },
 }
